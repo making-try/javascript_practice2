08270213956b36bd8e4329e67a1a258d830ca035
making-try <making.try@gmail.com>
2019-06-10 22:25:55 +0900

    20190610

--- main.js
+++ main.js
@@ -1,61 +1,72 @@
 document.addEventListener('DOMContentLoaded',function(){
+  let todo = document.getElementById('addBtn');
+  let status = document.getElementById('statusBtn');
+  let deletebtn = document.getElementById('deleteBtn');
+  //IDのカウンター
+  let counter = 0;
 
-    let getRadioValue = function(name){
-        let result = '';
-        let elements = document.getElementsByName(name);
-        for (let i = 0, len = elements.length; i < len; i++){
-            let elem = elements.item(i);
-            if (elem.checked){
-                result = elem.value;
-                break;
-            }
-        }
-        return result;
-    };
-
-    document.getElementById('addBtn').addEventListener('click',function(){
+  //ラジオボタンの値を取得
+  let getRadioValue = function(name){
+      let result = '';
+      let elements = document.getElementsByName(name);
+      for (let i = 0, len = elements.length; i < len; i++){
+          let elem = elements.item(i);
+          if (elem.checked){
+              result = elem.value;
+              break;
+          }
+      }
+      return result;
+  };
+  //追加ボタン押下時のアクション
+    todo.addEventListener('click',function(){
         const addTask = document.getElementById('addTask');
-        //const selectedAction = getRadioValue('task');
-        //alert(addTask.value);
-
-        //const output = document.getElementById('output');
-        //const anchor = document.createElement('p');
-
+        const output = document.getElementById('todoTable');
         /*while (output.firstChild){
             output.removeChild(output.firstChild);
         }*/
-        const output = document.getElementById('output');
-        
-        const tableAnchor = document.createElement('table');
-        const trAnchor = document.createElement('tr');
-        const thAnchor = document.createElement('th');
-        const thAnchor2 = document.createElement('th');
-        const thAnchor3 = document.createElement('th');
-        const headerText = document.createTextNode('ID');
-        const headerText2 = document.createTextNode('コメント');
-        const headerText3 = document.createTextNode('状態');
-
-        thAnchor.appendChild(headerText);
-        thAnchor2.appendChild(headerText2);
-        thAnchor3.appendChild(headerText3);
-
-        trAnchor.appendChild(thAnchor);
-        trAnchor.appendChild(thAnchor2);
-        trAnchor.appendChild(thAnchor3);
-
-        tableAnchor.appendChild(trAnchor);
-        //tableAnchor.appendChild(trAnchor2);
-
-        output.appendChild(tableAnchor);
-
 
         if (addTask.value !== ''){
-            //const output = document.getElementById('output');
-            const todoData = document.createElement('td');
-            const todoText = document.createTextNode(addTask.value)
-            anchor.appendChild(text);
-            output.appendChild(anchor);
-            result = NaN
+            const todoText = document.createTextNode(addTask.value);
+            const trAnchor = document.createElement('tr');
+            const idAnchor = document.createElement('td');
+            const idText = document.createTextNode(counter);
+            const commentAnchor = document.createElement('td');
+            const statusAnchor = document.createElement('td');
+
+            //作業中ボタンの作成
+            const statusbtnAnchor = document.createElement('input');
+            statusbtnAnchor.name = 'statusBtn';
+            statusbtnAnchor.type = 'button';
+            statusbtnAnchor.value = '作業中';
+            //削除ボタンの作成
+            const deletebtnAnchor = document.createElement('input');
+            deletebtnAnchor.name = 'deleteBtn';
+            deletebtnAnchor.type = 'button';
+            deletebtnAnchor.value = '削除';
+
+            //tdタグを組み立てる
+            idAnchor.appendChild(idText);
+            counter += 1;
+            commentAnchor.appendChild(todoText);
+            statusAnchor.appendChild(statusbtnAnchor);
+            statusAnchor.appendChild(deletebtnAnchor);
+            //trタグに組み立てたtdタグを代入する
+            trAnchor.appendChild(idAnchor);
+            trAnchor.appendChild(commentAnchor);
+            trAnchor.appendChild(statusAnchor);
+            //outputタグにtrタグを代入する
+            output.appendChild(trAnchor);
             }
+     });
+     //作業中ボタン押下時のアクション
+     /*
+     document.getElementById('statusBtn').addEventListener('click',function(){
+
+     });*/
+     //削除ボタン押下時のアクション
+     deletebtn.addEventListener('click',function(){
+       const output = document.getElementById('todoTable');
+       output.removeChild(output.firstChild);
      });
 })
