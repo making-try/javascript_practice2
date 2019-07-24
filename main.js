d07515c489418d157e18cba662d30a7d270c32ac
making-try <making.try@gmail.com>
2019-07-24 09:21:29 +0900

    変数名のキャメルケース表記への修正、関数の型letからconstへの修正

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
 document.addEventListener('DOMContentLoaded',function(){
-  const todo = document.getElementById('addBtn');
-  const radio_taskall = document.getElementById('task_all');
-  const radio_doing = document.getElementById('doing');
-  const radio_done = document.getElementById('done');
+  const taskAdd = document.getElementById('addBtn');
+  const radioTaskAll = document.getElementById('taskAll');
+  const radioDoing = document.getElementById('taskDoing');
+  const radioDone = document.getElementById('taskDone');
 
   //タスクを格納する配列
-  let task_list = [];
+  let taskLists = [];
 
   //ToDo表を再表示する関数
-  let review = function(arr){
-    const output = document.getElementById('todoTable');
+  const displayTasks = function(tasks){
+    const todoTable = document.getElementById('todoTable');
 
-    while (output.firstChild){
-      output.removeChild(output.firstChild);
+    while (todoTable.firstChild){
+      todoTable.removeChild(todoTable.firstChild);
     };
 
-    arr.forEach(function(value, index, ary){
-      viewTask(value[0], value[1], value[2],index, arr)
-
-      /*
-      value.forEach(function(value2, index2, ary2){
-        console.log(value2);
-        setAddTask(value2[0], index);
-      })*/
+    tasks.forEach(function(value, index, tasks){
+      viewTask(value[0], value[1], value[2],index, tasks)
     });
   }
 
-  let viewTask = function(taskId, value, status, index, ary){
-    const todoText = document.createTextNode(value);
+  const viewTask = function(taskId, taskValue, taskStatus, index, tasks){
+    const todoText = document.createTextNode(taskValue);
     const trAnchor = document.createElement('tr');
     trAnchor.id = 'tableRow' + taskId;
 
@@ -37,118 +31,117 @@ document.addEventListener('DOMContentLoaded',function(){
     const statusAnchor = document.createElement('td');
 
     //作業中ボタンの作成
-    const statusbtnAnchor = document.createElement('input');
-    statusbtnAnchor.id = 'statusBtn' + taskId;
-    statusbtnAnchor.type = 'button';
-    statusbtnAnchor.value = status;
+    const statusBtnAnchor = document.createElement('input');
+    statusBtnAnchor.id = 'statusBtn' + taskId;
+    statusBtnAnchor.type = 'button';
+    statusBtnAnchor.value = taskStatus;
     //削除ボタンの作成
-    const deletebtnAnchor = document.createElement('input');
-    deletebtnAnchor.id = 'deleteBtn' + taskId;
-    deletebtnAnchor.type = 'button';
-    deletebtnAnchor.value = '削除';
+    const deleteBtnAnchor = document.createElement('input');
+    deleteBtnAnchor.id = 'deleteBtn' + taskId;
+    deleteBtnAnchor.type = 'button';
+    deleteBtnAnchor.value = '削除';
 
     //tdタグを組み立てる
     idAnchor.appendChild(idText);
     commentAnchor.appendChild(todoText);
-    statusAnchor.appendChild(statusbtnAnchor);
-    statusAnchor.appendChild(deletebtnAnchor);
+    statusAnchor.appendChild(statusBtnAnchor);
+    statusAnchor.appendChild(deleteBtnAnchor);
     //trタグに組み立てたtdタグを代入する
     trAnchor.appendChild(idAnchor);
     trAnchor.appendChild(commentAnchor);
     trAnchor.appendChild(statusAnchor);
-    //outputタグにtrタグを代入する
+    //todoTableタグにtrタグを代入する
     todoTable.appendChild(trAnchor);
 
     //getElementById('deleteBtn'としないとEventListenできないが、
-    //常にdeletebtnには最後の要素が格納されている。しかし一番最初の要素の「削除ボタンも」
+    //常にdeleteBtnには最後の要素が格納されている。しかし一番最初の要素の「削除ボタンも」
     //押せるのはなぜだ？？
-    let deletebtn = document.getElementById('deleteBtn' + taskId);
-    let statusbtn = document.getElementById('statusBtn' + taskId);
+    let deleteBtn = document.getElementById('deleteBtn' + taskId);
+    let statusBtn = document.getElementById('statusBtn' + taskId);
 
-    deletebtn.addEventListener('click',function(){
+    deleteBtn.addEventListener('click',function(){
       //削除する行の行番号を削除ボタンのID名から取得し、配列を作り直す
-      let tableRow_id = deletebtn.id.slice(9)
-      ary_tmp = [];
-      task_list.forEach(function(value, index, ary){
-        if (value[0] !== Number(tableRow_id)){
-          ary_tmp.push(value);
+      let tableRowId = deleteBtn.id.slice(9)
+      tempTasks = [];
+      taskLists.forEach(function(value, index, tasks){
+        if (value[0] !== Number(tableRowId)){
+          tempTasks.push(value);
         }
       });
       //IDを振り直す
-      ary_tmp.forEach(function(value, index, ary){
+      tempTasks.forEach(function(value, index, tasks){
         value[0] = index;
       });
 
-      task_list = ary_tmp;
-      review(task_list);
+      taskLists = tempTasks;
+      displayTasks(taskLists);
     });
 
-    statusbtn.addEventListener('click',function(){
+    statusBtn.addEventListener('click',function(){
       //ステータスの行番号をボタンのID名から取得し、配列を作り直す
-      let tableRow_id = statusbtn.id.slice(9)
-      ary_tmp = [];
-      task_list.forEach(function(value, index, ary){
-        if (value[0] === Number(tableRow_id)){
+      let tableRowId = statusBtn.id.slice(9)
+      tempTasks = [];
+      taskLists.forEach(function(value, index, tasks){
+        if (value[0] === Number(tableRowId)){
           if (value[2] === '作業中'){
             value[2] = '完了';
           }else{
             value[2] = '作業中'
           }
         }
-        ary_tmp.push(value);
+        tempTasks.push(value);
       });
-      task_list = ary_tmp
-      review(task_list);
+      taskLists = tempTasks
+      displayTasks(taskLists);
     });
-
 
   };
 
   //追加ボタン押下時のアクション
-  todo.addEventListener('click',function(){
+  taskAdd.addEventListener('click',function(){
     const addTask = document.getElementById('addTask');
     //タスク名とタスクの状態を格納する配列[ID, タスク名, 状態]
-    let task_elem = []
-    let task_len = task_list.length
+    let taskElements = []
+    let taskLength = taskLists.length
 
     if (addTask.value !== ''){
-      task_elem.push(task_len);
-      task_elem.push(addTask.value);
-      task_elem.push('作業中');
+      taskElements.push(taskLength);
+      taskElements.push(addTask.value);
+      taskElements.push('作業中');
 
-      task_list.push(task_elem);
+      taskLists.push(taskElements);
 
-      review(task_list);
+      displayTasks(taskLists);
 
       addTask.value ='';
     }
   });
 
   //すべてボタン押下時のアクション
-  radio_taskall.addEventListener('click',function(){
-    review(task_list)
+  radioTaskAll.addEventListener('click',function(){
+    displayTasks(taskLists)
   });
 
   //作業中ボタン押下時のアクション
-  radio_doing.addEventListener('click',function(){
-    ary_tmp = [];
-    task_list.forEach(function(value, index, ary){
+  radioDoing.addEventListener('click',function(){
+    tempTasks = [];
+    taskLists.forEach(function(value, index, tasks){
       if (value[2] === '作業中'){
-        ary_tmp.push(value);
+        tempTasks.push(value);
       }
     });
-    review(ary_tmp);
+    displayTasks(tempTasks);
   });
 
   //完了ボタン押下時のアクション
-  radio_done.addEventListener('click',function(){
-    ary_tmp = [];
-    task_list.forEach(function(value, index, ary){
+  radioDone.addEventListener('click',function(){
+    tempTasks = [];
+    taskLists.forEach(function(value, index, tasks){
       if (value[2] === '完了'){
-        ary_tmp.push(value);
+        tempTasks.push(value);
       }
     });
-    review(ary_tmp);
+    displayTasks(tempTasks);
   });
 
 })
